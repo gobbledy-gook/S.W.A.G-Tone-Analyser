bfef74ec42980cdb6fbb4fb7560ecade56857308
AnsahMohammad <mohdansah10@gmail.com>
2023-03-03 14:59:32 +0530

    Added a local Tone analyzer

--- SWAG_Tone_Analyser.py
+++ SWAG_Tone_Analyser.py
@@ -4,6 +4,60 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 \
     import Features, CategoriesOptions, EmotionOptions, KeywordsOptions, EntitiesOptions
 
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import pandas as pd
+
+# Define hyperparameters
+vocab_size = 10000
+embedding_dim = 16
+max_length = 120
+trunc_type = 'post'
+padding_type = 'post'
+oov_token = '<OOV>'
+training_size = 35000
+
+data = pd.read_csv('training.csv')
+
+sentences = data['text']
+labels = data['label']
+
+# Split the data into training and testing sets
+training_sentences = sentences[:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[:training_size]
+testing_labels = labels[training_size:]
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+model2 = Sequential([
+    Embedding(vocab_size, embedding_dim, input_length=max_length),
+    LSTM(64),
+    Dense(1, activation='sigmoid')
+])
+
+# Compile the model2
+model2.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# Load the weights
+model2.load_weights('sentiment_model_weights.h5')
+
+def ask(sentance):
+    new_sentance = sentance
+
+    new_sentance = tokenizer.texts_to_sequences([new_sentance])
+    new_sentance = pad_sequences(new_sentance, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    return model2.predict(new_sentance)
+print("model loaded")
+
 authenticator = IAMAuthenticator(st.secrets["API"])
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
@@ -61,8 +115,9 @@ data = st.text_area('Analyse Your Text')
 
 if st.button('Analyse'):
     emotionsList, max_emotion, emo_dict = getEmotions(data)
+    print(data)
     st.success('Your text is analysed!')
-    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
     
     for i in range(len(emotionsList)):
         a = emotionsList[i][0]
@@ -103,4 +158,10 @@ if st.button('Analyse'):
     else:
         col6.write('NOT HATEFUL')
 
-
+    val = ask(data)
+    if val > 0.6:
+        col7.write("Positive Review 😊"+ str(val))
+    elif val < 0.4:
+        col7.write("Negative Review ☹️" + str(val))
+    else:
+        col7.write("Neutral 🙄" + str(val))
